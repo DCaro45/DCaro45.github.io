4ae6f726ad084db422efe36275cb2559031e1d0b
DCaro45 <148383769+DCaro45@users.noreply.github.com>
2024-09-11 23:23:02 +0100

    webpage + other stuff

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 // Code: JavaScript - Password Match
 document.addEventListener("DOMContentLoaded", () => {
-    var password1 = document.getElementById('password_1');
-    var password2 = document.getElementById('password_2');
-    var errorMessage = document.createElement('div');
-    errorMessage.className = 'error-message';
-    password2.parentNode.insertBefore(errorMessage, password2.nextSibling);
-
-    password2.addEventListener('input', function() {
-        if (password1.value !== password2.value) {
-            errorMessage.textContent = 'Passwords do not match!';
-        }
-        else {
-            errorMessage.textContent = '';
-        }
-    });
 
     const images = document.getElementsByClassName("bear");
     for(const image of images){
