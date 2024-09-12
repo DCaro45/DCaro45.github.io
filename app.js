c7979f522dda926edcdb3cec5cb8b4450ef1aafd
DCaro45 <148383769+DCaro45@users.noreply.github.com>
2024-09-12 23:17:25 +0100

    webpage

--- app.js
+++ app.js
@@ -11,4 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
             image.height = 100;
         })
     }
+
+    const form = document.getElementById("myForm");
+    const bioInput = document.getElementById("bio");
+    const errorMessage = document.createElement('div');
+    errorMessage.className = 'error-message';
+    bioInput.parentNode.insertBefore(errorMessage, bioInput.nextSibling);
+
+    form.addEventListener("submit", (event) => {
+        if (bioInput.value.trim() === "") {
+            event.preventDefault();
+            errorMessage.textContent = "Don't leave me empty!";
+            errorMessage.style.display = "block";
+        }
+        else {
+            errorMessage.style.display = "none"; // Hide warning message
+        }
+    });
 })
