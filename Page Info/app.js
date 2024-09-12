ab88ab193f75a6838df2c81ef45aff38609bc6d9
DCaro45 <148383769+DCaro45@users.noreply.github.com>
2024-09-12 19:48:19 +0100

    webpage

---
+++ Page Info/app.js
@@ -0,0 +1,14 @@
+// Code: JavaScript - Password Match
+document.addEventListener("DOMContentLoaded", () => {
+
+    const images = document.getElementsByClassName("bear");
+    for(const image of images){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            image.src = data.message
+            image.width = 100;
+            image.height = 100;
+        })
+    }
+})
