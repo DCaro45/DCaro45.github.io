ed0ca6edbb000382c1ae060b8684f0a0676ffc82
DCaro45 <carodavid854@gmail.com>
2024-09-21 22:35:37 +0100

    webpage

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 // Code: JavaScript - Password Match
 document.addEventListener("DOMContentLoaded", () => {
 
-    const images = document.getElementsByClassName("bear");
-    for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100;
-            image.height = 100;
-        })
-    }
-
     const form = document.getElementById("myForm");
     const bioInput = document.getElementById("bio");
     const errorMessage = document.createElement('div');
